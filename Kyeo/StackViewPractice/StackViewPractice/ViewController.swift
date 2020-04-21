b141b9a13afc44844cbc5b8baa9d5ed9dc908103
yeoyeoyoe <yeoyeoyoe@gmail.com>
2020-04-22 01:11:35 +0900

    add stackview project

---
+++ Kyeo/StackViewPractice/StackViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewPractice
+//
+//  Created by user42 on 2020/04/22.
+//  Copyright © 2020 Program42. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
