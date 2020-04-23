d2f8014893140896b6f98ebd523e73b74c001a7c
yeoyeoyoe <yeoyeoyoe@gmail.com>
2020-04-24 03:09:17 +0900

    add prototype

---
+++ Kyeo/PrototypeKyeo/PrototypeKyeo/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  PrototypeKyeo
+//
+//  Created by user42 on 2020/04/23.
+//  Copyright © 2020 Program42. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
