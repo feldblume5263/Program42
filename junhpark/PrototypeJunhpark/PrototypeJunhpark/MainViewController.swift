d453668b1485defe2bab2bdf59ab0c5aebecbbd8
feldblume5263 <53016167+feldblume5263@users.noreply.github.com>
2020-04-28 18:46:55 +0900

    홈 화면에 viewcell완성, 예시 3개 추가

--- junhpark/PrototypeJunhpark/PrototypeJunhpark/MainViewController.swift
+++ junhpark/PrototypeJunhpark/PrototypeJunhpark/MainViewController.swift
@@ -8,12 +8,44 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-   
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var userID = "JonyIve"
+    
+    
+    var missionName = ["study", "yoga", "running"]
+    var missionThumbnail = ["study", "yoga", "running"]
+    var missionDay = ["study" : "Mon, Tue, Wed, Thu, Fri", "yoga" : "Fri", "running" : "Sat, Sun" ]
+    var missionTime = ["study" : "11:00", "yoga" : "18:00", "running" : "09:00"]
+    
+    @IBOutlet var tableView: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView?.delegate = self
+        tableView?.dataSource = self
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "missionList", for: indexPath) as! MyMissionListTableViewCell
+        cell.nameLabel.text = missionName[indexPath.row]
+        cell.dayLabel.text = missionDay[missionName[indexPath.row]]
+        cell.timeLabel.text = missionTime[missionName[indexPath.row]]
+        cell.thumbnailImageView.image = UIImage(named: missionThumbnail[indexPath.row])
+
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return missionName.count
+    }
+    
+    
     
 }
 
