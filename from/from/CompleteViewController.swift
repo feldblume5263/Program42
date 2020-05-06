f7697a4a74e70d0fe54b463cb2cae9d0d55c2d16
feldblume5263 <53016167+feldblume5263@users.noreply.github.com>
2020-05-06 17:37:20 +0900

    위치 부분 수정

--- from/from/CompleteViewController.swift
+++ from/from/CompleteViewController.swift
@@ -39,52 +39,23 @@ class CompleteViewController: UIViewController, CLLocationManagerDelegate
     func setAnnotation(latitudeValue: CLLocationDegrees,
                        longitudeValue: CLLocationDegrees,
                        delta span :Double,
-                       title strTitle: String,
-                       subtitle strSubTitle:String)
+                       title strTitle: String)
     {
         let annotation = MKPointAnnotation()
         annotation.coordinate = goLocation(latitudeValue: latitudeValue, longtudeValue: longitudeValue, delta: span)
         annotation.title = strTitle
-        annotation.subtitle = strSubTitle
         compareMap.addAnnotation(annotation)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
         let pLocation = locations.last
+        
+        setAnnotation(latitudeValue: 37.488441, longitudeValue: 127.065112, delta: 0.1, title: "이노베이션아카데미")
+        
         _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!,
                        longtudeValue: (pLocation?.coordinate.longitude)!,
                        delta: 0.01)
-        setAnnotation(latitudeValue: 37.488441, longitudeValue: 127.065112, delta: 0.1, title: "이노베이션아카데미", subtitle: "서울 강남구 개포로 416")
-        CLGeocoder().reverseGeocodeLocation(
-            pLocation!, completionHandler: {(placemarks, error) -> Void in
-                let pm = placemarks!.first
-                let country = pm!.country
-                var address: String = ""
-                if country != nil {
-                    address = country!
-                }
-                if pm!.locality != nil {
-                    address += " "
-                    address += pm!.locality!
-                }
-                if pm!.thoroughfare != nil {
-                    address += " "
-                    address += pm!.thoroughfare!
-                }
-        }
-        )
         
-        func mapCircleView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer
-        {
-            if overlay.isKind(of: MKCircle.self){
-                let circleRenderer = MKCircleRenderer(overlay: overlay)
-                circleRenderer.fillColor = UIColor.blue.withAlphaComponent(0.1)
-                circleRenderer.strokeColor = UIColor.blue
-                circleRenderer.lineWidth = 1
-                return circleRenderer
-            }
-            return MKOverlayRenderer(overlay: overlay)
-        }
     }
 }
