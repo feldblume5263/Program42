bfa7e8393613854954031473565dc15577be6f5c
yeoyeoyoe <yeoyeoyoe@gmail.com>
2020-05-06 10:40:54 +0900

    from - CompleteViewController 추가

---
+++ from/from/CompleteViewController.swift
@@ -0,0 +1,90 @@
+//
+//  CompleteViewController.swift
+//  from
+//
+//  Created by 박준홍 on 2020/04/30.
+//  Copyright © 2020 42Seoul. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CompleteViewController: UIViewController, CLLocationManagerDelegate
+{
+    
+    @IBOutlet weak var compareMap: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        compareMap.showsUserLocation = true
+    }
+    
+    func goLocation(latitudeValue: CLLocationDegrees,
+                    longtudeValue: CLLocationDegrees,
+                    delta span: Double) -> CLLocationCoordinate2D
+    {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longtudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        compareMap.setRegion(pRegion, animated: true)
+        return pLocation
+    }
+    
+    func setAnnotation(latitudeValue: CLLocationDegrees,
+                       longitudeValue: CLLocationDegrees,
+                       delta span :Double,
+                       title strTitle: String,
+                       subtitle strSubTitle:String)
+    {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLocation(latitudeValue: latitudeValue, longtudeValue: longitudeValue, delta: span)
+        annotation.title = strTitle
+        annotation.subtitle = strSubTitle
+        compareMap.addAnnotation(annotation)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let pLocation = locations.last
+        _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!,
+                       longtudeValue: (pLocation?.coordinate.longitude)!,
+                       delta: 0.01)
+        setAnnotation(latitudeValue: 37.488441, longitudeValue: 127.065112, delta: 0.1, title: "이노베이션아카데미", subtitle: "서울 강남구 개포로 416")
+        CLGeocoder().reverseGeocodeLocation(
+            pLocation!, completionHandler: {(placemarks, error) -> Void in
+                let pm = placemarks!.first
+                let country = pm!.country
+                var address: String = ""
+                if country != nil {
+                    address = country!
+                }
+                if pm!.locality != nil {
+                    address += " "
+                    address += pm!.locality!
+                }
+                if pm!.thoroughfare != nil {
+                    address += " "
+                    address += pm!.thoroughfare!
+                }
+        }
+        )
+        
+        func mapCircleView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer
+        {
+            if overlay.isKind(of: MKCircle.self){
+                let circleRenderer = MKCircleRenderer(overlay: overlay)
+                circleRenderer.fillColor = UIColor.blue.withAlphaComponent(0.1)
+                circleRenderer.strokeColor = UIColor.blue
+                circleRenderer.lineWidth = 1
+                return circleRenderer
+            }
+            return MKOverlayRenderer(overlay: overlay)
+        }
+    }
+}
