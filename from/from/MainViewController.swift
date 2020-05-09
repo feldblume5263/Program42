5aa8645bdd77e2d2fc425ba5989b5444eb76f240
yeoyeoyoe <yeoyeoyoe@gmail.com>
2020-05-09 22:58:03 +0900

    위치 데이터 분리 구현

--- from/from/MainViewController.swift
+++ from/from/MainViewController.swift
@@ -87,6 +87,8 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         let cellIdentifier = "missionData"
         let cell: MainTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! MainTableViewCell
 
+        cell.completeButton.tag = indexPath.row
+        
         cell.selectionStyle = .none
         cell.completeButton.layer.cornerRadius = 5.0
         cell.missionName.text = missionData[indexPath.row]
@@ -162,13 +164,14 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
             {
                 return
             }
-            cvc.destLocation = locationData
+            let index = (sender as! UIButton).tag
+            cvc.destLocation = gpsData[index]
         }
     }
     
     @IBAction func pressCompleteButton(sender: UIButton)
     {
-        self.performSegue(withIdentifier: "toCompleteView", sender: self)
+        self.performSegue(withIdentifier: "toCompleteView", sender: sender)
     }
     
 }
